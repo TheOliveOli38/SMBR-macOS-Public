258d3fda9e53c5ef30a392703465acee8805c6b9
SkyanUltra <ultrathememer@gmail.com>
2025-11-12 18:30:50 -0500

    Fixed gravity inverting logic to prevent getting stuck
    The player's Y position will now get offset by the correct amount based on their power state, hitbox scale, and additionally accounts for if the player is crouching now, meaning all characters SHOULD no longer have issues with gravity inverter logic.

--- Scripts/Parts/GravityInverter.gd
+++ Scripts/Parts/GravityInverter.gd
@@ -11,11 +11,16 @@ func deactivate() -> void:
 	for i in get_tree().get_nodes_in_group("Players"):
 		on_player_exited(i)
 
+func calculate_player_height(player: Player):
+	var player_height = player.physics_params("HITBOX_SCALE")[1]
+	var player_crouch_height = player.physics_params("CROUCH_SCALE") if player.crouching else 1
+	return (16 if player.power_state.state_name == "Small" else 32) * player_height * player_crouch_height
+
 func on_player_entered(player: Player) -> void:
 	if player.gravity_vector == new_vector:
 		return
 	player.gravity_vector = new_vector
-	player.global_position.y -= 16
+	player.global_position.y -= calculate_player_height(player)
 	player.global_rotation = -player.gravity_vector.angle() + deg_to_rad(90)
 	player.get_node("CameraHandler").global_rotation = 0
 	player.get_node("CameraHandler").position.x = 0
@@ -26,7 +31,7 @@ func on_player_exited(player: Player) -> void:
 	if player.gravity_vector == Vector2.DOWN:
 		return
 	player.gravity_vector = Vector2.DOWN
-	player.global_position.y += 16
+	player.global_position.y += calculate_player_height(player)
 	player.velocity.y *= 1.1
 	player.global_rotation = -player.gravity_vector.angle() + deg_to_rad(90)
 	player.get_node("CameraHandler").position.x = 0
